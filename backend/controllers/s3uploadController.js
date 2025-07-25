7bc9540b6911b5fc9e98152de6a4c41c004cee40
Krish Khare <kharekrish049@gmail.com>
2025-07-25 15:06:47 +0530

    6commit

--- backend/controllers/s3uploadController.js
+++ backend/controllers/s3uploadController.js
@@ -20,8 +20,6 @@ const s3 = new S3Client({
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    // accessKeyId: 'AKIAUEYWR6JDUBRMJMHV',
-    // secretAccessKey: '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',
   },
 });
 
